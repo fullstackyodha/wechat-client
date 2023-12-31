29a5980cc418f7a41ea23541f3f52a0568ab5320
Harshal Solanki <harshal3196@gmail.com>
2023-12-31 18:52:54 +0530

    Minor Updates in develop

--- src/redux-toolkit/reducers/user/user.reducer.js
+++ src/redux-toolkit/reducers/user/user.reducer.js
@@ -1,3 +1,21 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = { token: "", profile: null };
+
+// generates action creators and action types that correspond to the reducers and state.
+const userSlice = createSlice({
+    name: "User",
+    initialState,
+    reducers: {
+        addUser: (state, action) => {
+            const { token, profile } = action.payload;
+            state.token = token;
+            state.profile = profile;
+        }
+    }
+});
+
+// ACTION
+export const { addUser } = userSlice.actions;
+// REDUCERS
+export default userSlice.reducer;
--- src/redux-toolkit/store.js
+++ src/redux-toolkit/store.js
@@ -1,3 +1,4 @@
 import { configureStore } from "@reduxjs/toolkit";
+import userReducer from "@redux/reducers/user/user.reducer";
 
-export const store = configureStore({ reducer: {} });
+export const store = configureStore({ reducer: { user: userReducer } });

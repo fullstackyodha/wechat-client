0cab5a4914c9586cf9cfdd3d505a097e83648a4d
fullstackyodha <harshal3196@gmail.com>
2024-12-07 23:19:06 +0530

    Added toast component

--- src/components/toast/Toast.jsx
+++ src/components/toast/Toast.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 import "@/components/toast/Toast.scss";
+import PropTypes from "prop-types";
 
-const Toast = () => {
+function Toast(props) {
+    const { toastLife, position } = props;
+
     return <div>Toast</div>;
-};
+}
 
+Toast.PropTypes = {};
 export default Toast;
